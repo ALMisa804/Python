ff39052bb8ba5eef7eba0ecc42b70dd81e802466
Al Misa <73444369+ALMisa804@users.noreply.github.com>
2021-01-03 23:15:39 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+# Create a Python script that analyzes the votes and calculates:
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote
+
+# Import dependencies
+import os
+import csv
+
+# Define variables
+candidates = {}
+
+# Path to collect data
+election_csv = os.path.join('.','election_data.csv')
+with open(election_csv, 'r') as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    for row in csvreader:
+        if row[2] in candidates.keys():
+            candidates[row[2]]+=1
+        else:
+            candidates[row[2]] = 1
+
+        total = candidates.values()
+        total_votes = sum(total)
+
+            
+        list_candidates = candidates.keys()
+            
+        votes_per = [f'{(x/total_votes)*100:.3f}%' for x in candidates.values()]
+            
+
+        winner = list(candidates.keys())[list(candidates.values()).index(max(candidates.values()))]
+        winner
+        
+
+
+print("Election results")
+
+print("--------------------------------")
+
+print(f" Total votes: {int(total_votes)}")
+
+print("---------------------------------")
+i = 0
+for candidate, vote in candidates.items():
+    print(f'{candidate}, {vote} , {votes_per[i]}') 
+    i+=1
+print("------------------------------")
+
+print(f" Winner: {winner}")
+
+print("------------------------------")
